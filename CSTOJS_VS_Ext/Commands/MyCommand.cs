6ce2c6e52497ffac6b169c3827f88a9ee8fbd359
TiLied <TiLied@users.noreply.github.com>
2023-09-28 14:16:44 +0300

    Add project files.

---
+++ CSTOJS_VS_Ext/Commands/MyCommand.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.VisualStudio.Imaging;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace CSTOJS_VS_Ext
+{
+	[Command(PackageIds.MyCommand)]
+	internal sealed class MyCommand : BaseCommand<MyCommand>
+	{
+		protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+		{
+			PhysicalFile file = await VS.Solutions.GetActiveItemAsync() as PhysicalFile;
+
+			if (file == null)
+			{
+				var model = new InfoBarModel(new[] {
+					new InfoBarTextSpan("File path is null! Save it first!")
+				}, KnownMonikers.PlayStepGroup,	true);
+
+				InfoBar infoBar = await VS.InfoBar.CreateAsync(ToolWindowGuids80.SolutionExplorer, model);
+				await infoBar.TryShowInfoBarUIAsync();
+				return;
+			}
+
+			string strPath = file.FullPath;
+
+			if(!strPath.EndsWith(".cs"))
+			{
+				var model = new InfoBarModel(new[] {
+					new InfoBarTextSpan("File is NOT CSHARP!")
+				}, KnownMonikers.PlayStepGroup, true);
+
+				InfoBar infoBar = await VS.InfoBar.CreateAsync(ToolWindowGuids80.SolutionExplorer, model);
+				await infoBar.TryShowInfoBarUIAsync();
+				return;
+			}
+
+			string directory = Path.GetDirectoryName(strPath);
+
+			General options = await General.GetLiveInstanceAsync();
+			
+			Process p = new();
+
+			p.StartInfo.UseShellExecute = false;
+			p.StartInfo.RedirectStandardOutput = true;
+			p.StartInfo.RedirectStandardError = true;
+			p.StartInfo.CreateNoWindow = true;
+
+			if (options.PathToCSTOJS_CLI != null && options.PathToCSTOJS_CLI != "") 
+				p.StartInfo.FileName = options.PathToCSTOJS_CLI;
+			else
+				p.StartInfo.FileName = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\CSTOJS_CLI\\CSTOJS_CLI.exe";
+
+			List<string> args = new(){ "f", $"\"{strPath}\"", "-OutPutPath", $"\"{directory}\"", "-DisableConsoleColors", "true" };
+
+			if (options.Debug) 
+			{
+				args.Add("-Debug");
+				args.Add($"true");
+			}
+
+			p.StartInfo.Arguments = string.Join(" ", args);
+
+			try
+			{
+				p.Start();
+			}
+			catch (Exception ex)
+			{
+				ex.Log();
+			}
+			
+			while (!p.StandardError.EndOfStream)
+			{
+				string line = await p.StandardError.ReadLineAsync();
+				await CSTOJS_VS_ExtPackage.Pane.WriteLineAsync(line);
+			}
+			while (!p.StandardOutput.EndOfStream)
+			{
+				string line = await p.StandardOutput.ReadLineAsync();
+				await CSTOJS_VS_ExtPackage.Pane.WriteLineAsync(line);
+			}
+
+			p.WaitForExit();
+		}
+	}
+}
---
+++ CSTOJS_VS_Ext/Options/General.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace CSTOJS_VS_Ext
+{
+	internal partial class OptionsProvider
+	{
+		// Register the options with this attribute on your package class:
+		// [ProvideOptionPage(typeof(OptionsProvider.GeneralOptions), "VSIXProjectTest", "General", 0, 0, true, SupportsProfiles = true)]
+		[ComVisible(true)]
+		public class GeneralOptions : BaseOptionPage<General> { }
+	}
+
+	public class General : BaseOptionModel<General>
+	{
+		[Category("Extension options")]
+		[DisplayName("Path To CSTOJS_CLI")]
+		[Description("Path To CSTOJS_CLI. Default is null(empty string), means using Embedded CSTOJS_CLI.")]
+		[DefaultValue("")]
+		public string PathToCSTOJS_CLI { get; set; } = string.Empty;
+
+		[Category("CSharpToJavaScript options")]
+		[DisplayName("Debug")]
+		[Description("Debug. When set to true prints additional info to console, cs lines to js file and creates file Debug.txt,")]
+		[DefaultValue(false)]
+		public bool Debug { get; set; } = false;
+	}
+}
